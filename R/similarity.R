870cf5f7e66ef829059f05268a85f335a8ce6d0c
Vaclav Hausenblas <vaclav.hausenblas@gmail.com>
2017-01-11 14:11:15 +0100

    aggSimilarity can be calculated on similarity metrix new arcCosDist() and arcCosSimilarity()

--- R/similarity.R
+++ R/similarity.R
@@ -14,19 +14,39 @@ upperTMean <- function(inmat, weights = NULL){
 #'
 #' \code{aggSimilarity} computes aggregate similarity of banks' portfolios.
 #' @param portfolios a (n x k) matrix of portfolios where rows represent n subjects and columns their portfolios
+#' @param simM an optional (n x n) similarity matrix
 #' @param weights a (n x n) matrix of weights for weghting matrix to an average
 #' @param rw (1 x k) vector of weights on sectors
-#' @details This is an R version of the function.
+#' @param arc logical; should the cosine similarities be transformed to radians (by arccos) first?
+#' @details This is an R version of the function. Using \code{arc=TRUE} returns (weighted) mean of 1-(2*arccos(cos(similarity))/pi)
 #' @return a numeric value of (weighted) mean similarity.
 #' @seealso \code{\link{aggSimilarity_time}}
-aggSimilarity <- function(porftolios, weights = NULL, rw = NULL){
+aggSimilarity <- function(
+  porftolios, 
+  simM    = NULL, 
+  weights = NULL, 
+  rw      = NULL, 
+  arc     = FALSE)
+{
+  
+  if (is.null(simM)) {
     if (is.null(rw)) {
       tmpm <- mCosSimilarity(porftolios)
     } else {
       tmpm <- wtMCosSimilarity(porftolios, weights = rw)
     }
-    return(upperTMean(tmpm, weights))
+  } else {
+    tmpm <- simM
+  }
+  
+  if (arc) {
+    tmpm[tmpm > 1 & tmpm < 1 + 2*.Machine$double.eps] <- 1
+    tmpm <- 1 - arcCosDist(tmpm)
+  } 
+  
+  return(upperTMean(tmpm, weights))
 }
+
 
 #' Aggregate similarities of banking porfolios on a list
 #'
@@ -39,3 +59,18 @@ aggSimilarity <- function(porftolios, weights = NULL, rw = NULL){
 aggSimilarity_time <- function(portfolios_list, ...) {
   return(sapply(portfolios_list, aggSimilarity, ...))
 }
+
+#' arccos distance transformed to [0;1] scale 
+#' @param x cosine similarities of positive vectors (as a vector or matrix)
+#' @return angular distance
+arcCosDist <- function(x) {
+  return(2*acos(x)/pi)
+}
+
+#' arccos distance transformed to [0;1] scale 
+#' @param x cosine similarities of positive vectors (as a vector or matrix)
+#' @return angular distance
+arcCosSimilarity <- function(input_mat) {
+  return(1-arcCosDist(mCosSimilarity(input_mat)))
+}
+
